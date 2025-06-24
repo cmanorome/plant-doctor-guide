5524ffbbda8e3f580827a8b3cc9a5135b700bbc7
Chaundra <marketing@Chaundras-MacBook-Air.local>
2025-06-24 14:04:11 +1000

    Add iframe headers to allow embedding in Maropost Commerce

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL"
+          },
+          {
+            key: "Content-Security-Policy", 
+            value: "frame-ancestors 'self' https://plantdoctor.com.au https://*.plantdoctor.com.au https://*.maropost.com https://*.neto.com.au *"
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
